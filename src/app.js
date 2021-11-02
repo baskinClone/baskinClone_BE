4db5cca2985223f388664b0488dd33ce791025e6
LST <wer1264@gmail.com>
2021-11-03 00:55:35 +0900

    feat: 아이스크림 정보 크롤링
    MONTHLY BEST ICECREAM, ICECREAM 정보 크롤링

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 require("dotenv").config();
 require("./db");
 
+const Icecream = require("./models/icecream");
+
 const app = express();
 const logger = morgan("dev");
 
@@ -12,6 +14,15 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(logger);
 app.use(express.json());
+
+app.use("/", async (req, res, next) => {
+  const result = await Icecream.saveDocs({
+    rank: "1위",
+    imgurl: "aaa",
+    name: "aaa",
+  });
+  return res.json(result);
+});
 
 app.listen(PORT, () => {
   console.log(`✅ Server listening on http://localhost:${PORT}`);
